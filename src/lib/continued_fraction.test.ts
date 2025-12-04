fed65fcf3793eb72cd925700370e50452011257c
wasd314 <68897049+wasd314@users.noreply.github.com>
2025-12-05 07:29:38 +0900

    impl descend, toSternBrocotAncients

--- src/lib/continued_fraction.test.ts
+++ src/lib/continued_fraction.test.ts
@@ -1,5 +1,8 @@
 import { expect, test } from "vitest";
-import { toContinuedFraction } from "./continued_fraction";
+import {
+  toContinuedFraction,
+  toSternBrocotAncients,
+} from "./continued_fraction";
 
 test("toContinuedFraction", () => {
   [
@@ -14,4 +17,41 @@ test("toContinuedFraction", () => {
   ].forEach(({ numerator, denominator, cf }) => {
     expect(toContinuedFraction({ numerator, denominator })).toStrictEqual(cf);
   });
+});
+
+test("toSternBrocotAncients", () => {
+  expect(
+    toSternBrocotAncients({ numerator: 1n, denominator: 1n }),
+  ).toStrictEqual([
+    {
+      begin: { numerator: 1n, denominator: 1n },
+      diff: { numerator: 1n, denominator: 0n },
+      length: 0n,
+    },
+  ]);
+
+  expect(
+    toSternBrocotAncients({ numerator: 5n, denominator: 1n }),
+  ).toStrictEqual([
+    {
+      begin: { numerator: 1n, denominator: 1n },
+      diff: { numerator: 1n, denominator: 0n },
+      length: 4n,
+    },
+  ]);
+
+  expect(
+    toSternBrocotAncients({ numerator: 1n, denominator: 5n }),
+  ).toStrictEqual([
+    {
+      begin: { numerator: 1n, denominator: 1n },
+      diff: { numerator: 1n, denominator: 0n },
+      length: 0n,
+    },
+    {
+      begin: { numerator: 1n, denominator: 1n },
+      diff: { numerator: 0n, denominator: 1n },
+      length: 4n,
+    },
+  ]);
 });
--- src/lib/continued_fraction.ts
+++ src/lib/continued_fraction.ts
@@ -13,3 +13,42 @@ export const toContinuedFraction = ({
   }
   return cf;
 };
+
+interface DescendProps {
+  begin: Fraction;
+  diff: Fraction;
+  length: bigint;
+}
+export const descend = ({ begin, diff, length }: DescendProps): Fraction => {
+  return {
+    numerator: begin.numerator + diff.numerator * length,
+    denominator: begin.denominator + diff.denominator * length,
+  };
+};
+
+/**
+ * 系列 begin (+) i (*) diff (0 <= i <= length)
+ * ただし (+), (*) は分母・分子ごとに
+ */
+interface Path {
+  begin: Fraction;
+  diff: Fraction;
+  length: bigint;
+}
+
+export const toSternBrocotAncients = (frac: Fraction) => {
+  const cf = toContinuedFraction(frac);
+  cf[cf.length - 1]--;
+  const paths: Path[] = [];
+  let begin: Fraction = { numerator: 1n, denominator: 1n };
+  let diff: Fraction = { numerator: 1n, denominator: 0n };
+  cf.forEach((length) => {
+    paths.push({ begin, diff, length });
+    const newBegin = descend({ begin, diff, length });
+    const newDiff = descend({ begin, diff, length: length - 1n });
+    begin = newBegin;
+    diff = newDiff;
+  });
+
+  return paths;
+};
