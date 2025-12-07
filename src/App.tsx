85c75eeae64ed2e4d3bf515b03fb062d5d0202a6
wasd314 <68897049+wasd314@users.noreply.github.com>
2025-12-07 16:35:36 +0900

    refactor Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
-import Container from "./Visualizer";
+import { type SyntheticEvent, useState } from "react";
+import Visualizer from "./Visualizer";
 import "./App.css";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Tab from "@mui/material/Tab";
+
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+  typography: {
+    button: {
+      textTransform: "none",
+    },
+  },
+});
 
 function App() {
+  const [value, setValue] = useState("1");
+
+  const handleValue = (_event: SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
   return (
-    <>
-      <Container />
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box sx={{ width: "100%" }}>
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <TabList onChange={handleValue} aria-label="function selection">
+              <Tab label="Path" value="1" />
+              <Tab label="Settings" value="2" />
+            </TabList>
+          </Box>
+          <TabPanel value="1" keepMounted>
+            <Visualizer />
+          </TabPanel>
+          <TabPanel value="2"></TabPanel>
+        </TabContext>
+      </Box>
+    </ThemeProvider>
   );
 }
 
--- src/Visualizer.tsx
+++ src/Visualizer.tsx
@@ -1,13 +1,6 @@
-import TabContext from "@mui/lab/TabContext";
-import TabList from "@mui/lab/TabList";
-import TabPanel from "@mui/lab/TabPanel";
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Tab from "@mui/material/Tab";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -15,25 +8,14 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import TextField from "@mui/material/TextField";
-import { type SyntheticEvent, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import {
   enumerateAncients,
   toContinuedFraction,
 } from "./lib/continued_fraction";
 import { type Fraction, parseFraction } from "./lib/parse";
 
-const theme = createTheme({
-  colorSchemes: {
-    dark: true,
-  },
-  typography: {
-    button: {
-      textTransform: "none",
-    },
-  },
-});
-
-export const Visualizer = () => {
+const Visualizer = () => {
   const [frac, setFrac] = useState<Fraction>({ num: 1n, den: 1n });
   const inputRef = useRef<HTMLInputElement>(null);
   const handleClick = () => {
@@ -140,31 +122,4 @@ export const Visualizer = () => {
   );
 };
 
-const Container = () => {
-  const [value, setValue] = useState("1");
-
-  const handleValue = (_event: SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-  };
-
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Box sx={{ width: "100%" }}>
-        <TabContext value={value}>
-          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <TabList onChange={handleValue} aria-label="function selection">
-              <Tab label="Path" value="1" />
-              <Tab label="Settings" value="2" />
-            </TabList>
-          </Box>
-          <TabPanel value="1" keepMounted>
-            <Visualizer />
-          </TabPanel>
-          <TabPanel value="2"></TabPanel>
-        </TabContext>
-      </Box>
-    </ThemeProvider>
-  );
-};
-export default Container;
+export default Visualizer;
